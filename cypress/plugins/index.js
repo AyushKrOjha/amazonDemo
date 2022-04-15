8bd004ea694386b970043ca3f8a1af1dde1c10bd
AyushKrOjha <ayush2394@outlook.com>
2022-04-15 11:48:28 +0530

    Add env files

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -10,7 +10,6 @@
 // ***********************************************************
 
 const cucumber = require('cypress-cucumber-preprocessor').default
-
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
